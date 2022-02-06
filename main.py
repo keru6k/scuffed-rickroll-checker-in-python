19c657f19e407fdc28845d7f83c2dc2361e2bf23
kerthegreat <bacototker@gmail.com>
2022-02-06 21:11:33 +0800

    Condition problem fixed

---
+++ main.py
@@ -0,0 +1,10 @@
+#made with luv by k6r <3
+rick = None
+rick = input("please paste the link. ")
+if ((rick == "https://www.youtube.com/watch?v=dQw4w9WgXcQ") or (rick == "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")):
+    print("Be careful! This is a rickroll!")
+    print("You are advised to not click the link!!")
+    input("press any key to end the program. ")
+else:
+    print("Hooray! this is not a rickroll! You're safe, for now.")
+    input("press any key to end the program. ")
